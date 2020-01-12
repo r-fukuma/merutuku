6e18170ac86189ec1aec42b7833d0def42a6386c
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2020-01-12 23:30:20 +0900

    nuxtさせる部分を変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,32 @@
 <template>
   <el-container>
     <el-header class="header" height="50px">
-      <p>メルつく</p>
+      <i class="el-icon-message" style="vertical-align: middle"></i>
+      merutuku
     </el-header>
-    <nuxt />
+    <el-container>
+      <el-aside width="400px">
+        <side-menu />
+      </el-aside>
+      <el-main style="padding:0 20px">
+        <nuxt />
+      </el-main>
+    </el-container>
   </el-container>
 </template>
+<script>
+export default {
+  components: {
+    SideMenu: () => import('~/components/TheSideMenu.vue')
+  }
+}
+</script>
 
 <style>
 .header {
-  background-color: #64bdd4;
-  text-align: center;
+  background-color: #478899;
   color: white;
   font-size: 32px;
-  font-weight: bold;
 }
 * {
   margin: 0;
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,10 @@
 <template>
-  <el-container>
-    <el-aside width="400px">
-      <side-menu />
-    </el-aside>
-    <el-main style="padding:0 20px">
-      <mail-form />
-    </el-main>
-  </el-container>
+  <mail-form />
 </template>
 
 <script>
 export default {
   components: {
-    SideMenu: () => import('~/components/TheSideMenu.vue'),
     MailForm: () => import('~/components/TheMailForm.vue')
   }
 }
